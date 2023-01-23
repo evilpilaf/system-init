a581319b91431144bc171eb60a189c31b7519551
evilpilaf <mauriciod73@gmail.com>
2023-01-23 22:43:12 +0100

    fix path to setup-powershell and check for default wsl install

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,9 +1,17 @@
-Write-Host "Before we start, here's a few question"
+# Write-Host "Before we start, here's a few question"
 
-$streaming = Read-Host "Setup streaming? (y/n)"
-$desktop = Read-Host "Setup desktop? (y/n)"
+# $streaming = Read-Host "Setup streaming? (y/n)"
+# $desktop = Read-Host "Setup desktop? (y/n)"
 
-wsl --install
+$wslDefault = 'Ubuntu-22.04'
+
+$wslInput = Read-Host "Linux version (Default: Ubuntu-22.04)"
+
+if ($null -eq $wslInput) {
+    $wslInput = $wslDefault
+}
+
+wsl --install -d $wslInput
 
 Write-Host Installing winget packages
 
@@ -14,8 +22,8 @@ $packages = @(
     'LINQPad.LINQPad.7',
     'Microsoft.WindowsTerminal.Preview',
     'Docker.DockerDesktop',
-    'suse.RancherDesktop',
-    'icsharpcode.ILSpy',
+    # 'suse.RancherDesktop',
+    # 'icsharpcode.ILSpy',
     'JanDeDobbeleer.OhMyPosh',
     'ScooterSoftware.BeyondCompare4',
     'Postman.Postman',
@@ -69,22 +77,5 @@ $packages | ForEach-Object { winget install --id $_ --source winget }
 
 Write-Host Setting up PowerShell
 
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/aaronpowell/system-init/main/windows/setup-powershell.ps1'))
-pwsh -c "Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/aaronpowell/system-init/main/windows/setup-powershell.ps1'))"
-
-Write-Host Manuall install the following
-Write-Host "- Visual Studio DF"
-Write-Host "- Edge Canary"
-Write-Host "- caskaydiacove nf: https://www.nerdfonts.com/font-downloads"
-
-if ($streaming -eq "y") {
-    Write-Host OBS Plugins
-    Write-Host "- Stream Elements"
-    Write-Host "- Advanced Scene Switcher"
-    Write-Host "- OBS WebSockets"
-    Write-Host "- StreamFX"
-}
-
-if ($desktop -ne "y") {
-    Write-Host Remember to Update path for oh-my-posh
-}
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/evilpilaf/system-init/main/windows/setup-powershell.ps1'))
+pwsh -c "Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/evilpilaf/system-init/main/windows/setup-powershell.ps1'))"
